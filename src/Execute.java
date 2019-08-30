f24cf9d2f3b52f42155c2d8e15d2c7e7b170340d
cindy1u0 <luo.xiny@husky.neu.edu>
2019-08-30 04:29:05 -0400

    commit

--- src/Execute.java
+++ src/Execute.java
@@ -9,6 +9,7 @@ public final class Execute {
   public static void main(String[] args) {
     Polyomino model = new PolyominoImpl();
     PolyominoView view = new PolyominoViewImpl();
+    model.randGenSolution(7);
     PolyominoControl controller = new PolyominoControlImpl(model, view);
     controller.run();
   }
